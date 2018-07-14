eac55ea7886b11f96bf6fd42230886a5740b1cd4
Snow <polarpenguin@foxmail.com>
2018-07-15 03:44:21 +0800

    Add macro for deploying on android

--- main.cpp
+++ main.cpp
@@ -6,8 +6,12 @@ int main(int argc, char *argv[])
     QApplication a(argc, argv);
     MainWindow w;
     w.setWindowTitle(u8"ClassmateNameEncoder");
+#ifdef Q_OS_ANDROID
+    w.showMaximized();
+#else
     w.resize(250,155);
     w.show();
+#endif
 
     return a.exec();
 }
